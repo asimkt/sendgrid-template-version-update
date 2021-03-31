3ea27abfc8c5669622b038d1e03a544db208c842
Asim K T <asim@rocketlane.com>
2021-03-31 19:26:01 +0530

    update-one-template at a time

--- index.js
+++ index.js
@@ -6,47 +6,38 @@ const http = require("https");
 try {
     // `who-to-greet` input defined in action metadata file
     const apiKey = core.getInput('api-key');
-    console.log(`API KEY: ${apiKey}!`);
+    const templateVersionId = core.getInput('template-version');
+    const templateContent = core.getInput('template-content');
+    
+    console.log(templateVersionId);
+    var options = {
+        "method": "PATCH",
+        "hostname": "api.sendgrid.com",
+        "port": null,
+        "path": `/v3/templates/${templateVersionId}`,
+        "headers": {
+            "authorization": `Bearer ${apiKey}`,
+            "content-type": "application/json"
+        }
+    };
 
-    const idMap = core.getInput('id-content-map');
-    const map = JSON.parse(idMap);
-    console.log(`the map is`, Object.keys(map));
-    const time = (new Date()).toTimeString();
-    core.setOutput("time", time);
-    // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    //   console.log(`The event payload: ${payload}`);
+    var req = http.request(options, function (res) {
+        var chunks = [];
 
-    Object.keys(map).forEach(function(templateVersionId) {
-        var options = {
-            "method": "PATCH",
-            "hostname": "api.sendgrid.com",
-            "port": null,
-            "path": `/v3/templates/${templateVersionId}`,
-            "headers": {
-                "authorization": `Bearer ${apiKey}`,
-                "content-type": "application/json"
-            }
-        };
-    
-        var req = http.request(options, function (res) {
-            var chunks = [];
-    
-            res.on("data", function (chunk) {
-                chunks.push(chunk);
-            });
-    
-            res.on("end", function () {
-                var body = Buffer.concat(chunks);
-                console.log(body.toString());
-            });
+        res.on("data", function (chunk) {
+            chunks.push(chunk);
         });
-    
-        req.write(JSON.stringify({
-            html_content: map[templateVersionId]
-        }));
-        req.end();  
-    })
+
+        res.on("end", function () {
+            var body = Buffer.concat(chunks);
+            console.log(body.toString());
+        });
+    });
+
+    req.write(JSON.stringify({
+        html_content: templateContent
+    }));
+    req.end();  
 } catch (error) {
     core.setFailed(error.message);
 }
